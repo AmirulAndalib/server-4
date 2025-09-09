c3e0bf07d873da527fb5b60aa70b4c911f5c48c8
Maxim Raznatovski <nda.mr43@gmail.com>
2025-09-09 13:41:03 +0200

    Only change env in `run-in-env.sh` when not inside a devcontainer

--- scripts/run-in-env.sh
+++ scripts/run-in-env.sh
@@ -1,22 +1,27 @@
 #!/usr/bin/env sh
 set -eu
 
-# Activate pyenv and virtualenv if present, then run the specified command
 
-# pyenv, pyenv-virtualenv
-if [ -s .python-version ]; then
-    PYENV_VERSION=$(head -n 1 .python-version)
-    export PYENV_VERSION
+if [ -z "${DEV_CONTAINER:-}" ]; then
+  # In devcontainers the environment is already activated in the Dockerfile.
+  # For everything else, activate pyenv and virtualenv if present:
+
+  # pyenv, pyenv-virtualenv
+  if [ -s .python-version ]; then
+      PYENV_VERSION=$(head -n 1 .python-version)
+      export PYENV_VERSION
+  fi
+
+  # other common virtualenvs
+  my_path=$(git rev-parse --show-toplevel)
+
+  for venv in venv .venv .; do
+    if [ -f "${my_path}/${venv}/bin/activate" ]; then
+      . "${my_path}/${venv}/bin/activate"
+      break
+    fi
+  done
 fi
 
-# other common virtualenvs
-my_path=$(git rev-parse --show-toplevel)
-
-for venv in venv .venv .; do
-  if [ -f "${my_path}/${venv}/bin/activate" ]; then
-    . "${my_path}/${venv}/bin/activate"
-    break
-  fi
-done
-
+# And then run the specified command:
 exec "$@"
